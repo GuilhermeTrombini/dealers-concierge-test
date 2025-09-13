4be5f2d01c288806352c7a2fe3e5821f49e18ec7
Guilherme Trombini <agi_ggerazo@contractor.indeed.com>
2025-09-13 20:09:18 -0300

    fix: updated env urls

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -18,7 +18,11 @@ const yoga = createYoga({
     prisma,
   },
   cors: {
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: [
+      process.env.FRONTEND_URL || 'http://localhost:5173',
+      'http://localhost:5173',
+      'https://dealers-concierge-test.fly.dev'
+    ],
     credentials: true,
   },
 })
