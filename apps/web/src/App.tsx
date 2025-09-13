395fd2a5f1664efa7e9f26ef179a04db9eeffb88
Guilherme Trombini <agi_ggerazo@contractor.indeed.com>
2025-09-13 17:22:18 -0300

    feat(web): add React application core
    - Add React application entry point and routing
    - Add global CSS styles with Tailwind
    - Add Vite environment type definitions
    - Add React Router for navigation
    - Set up application structure

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from 'react-router-dom'
+import { CasesList } from './routes/CasesList'
+import { CaseDetail } from './routes/CaseDetail'
+import { Layout } from './components'
+
+function App() {
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<CasesList />} />
+        <Route path="/cases/:id" element={<CaseDetail />} />
+      </Routes>
+    </Layout>
+  )
+}
+
+export default App
+
+
